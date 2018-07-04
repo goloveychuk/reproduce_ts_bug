d0bb42eeb7148a6be4d8a7769a8406b8a49a93c7
Vadym Holoveichuk <goloveychuk1@gmail.com>
2018-07-04 14:59:21 +0300

    init

---
+++ test.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var lodash_1 = require("lodash");
+exports.mapDispatchToProps = lodash_1.mapValues({}, function () { });
---
+++ test.ts
@@ -0,0 +1,2 @@
+import {mapValues} from 'lodash';
+export const mapDispatchToProps = mapValues({}, ()=>{})
